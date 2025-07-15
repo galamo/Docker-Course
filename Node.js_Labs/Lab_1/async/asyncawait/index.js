c2a6cb1df6731dc26a368909f8fab05295e23aab
Gal Amouyal <galamouy@Gals-MacBook-Pro.local>
2025-07-15 11:17:42 +0300

    a

---
+++ Node.js_Labs/Lab_1/async/asyncawait/index.js
@@ -0,0 +1,21 @@
+const axios = require("axios");
+
+async function callApi1() {
+  console.log("455");
+  const result = await axios.get("https://currentPage1.com");
+  console.log("333");
+}
+
+async function callApi2() {
+  console.log("451");
+  const result = await axios.get("https://currentPage2.com");
+  console.log("1212");
+}
+
+console.log("666");
+callApi2();
+callApi1();
+console.log("888");
+
+// solution 
+// 666 => 451 => 455 => 888 => 1212 || 333
